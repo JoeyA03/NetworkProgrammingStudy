7ca4b6d87eb5ce4de927439ca27ae0ec94423e09
JoeyA03 <Joey_a03@hotmail.com>
2023-09-21 15:13:37 -0400

    Added Movement
    Added movement for the player to use when connected to the host

--- NetworkProgrammingStudy/Assets/Scripts/CharacterController.cs
+++ NetworkProgrammingStudy/Assets/Scripts/CharacterController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDir = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if(Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if(Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if(Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
     }
 }
